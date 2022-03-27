b79f42a0452559b303cc980392e6d23643b7a72f
Eugene Dubovik <dev.dubovik@gmail.com>
2022-03-27 18:51:37 +0300

    create Text

---
+++ src/main/java/com/epam/jwd/entity/paragraph/Text.java
@@ -0,0 +1,47 @@
+package com.epam.jwd.entity.paragraph;
+
+import com.epam.jwd.entity.TextElement;
+
+import java.util.List;
+
+public class Text implements TextElement {
+    private List<TextElement> allTextElements;
+
+    public Text(List<TextElement> paragraphs) {
+        this.allTextElements = paragraphs;
+    }
+
+    public void setParagraphs(List<TextElement> paragraphs) {
+        this.allTextElements = paragraphs;
+    }
+
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Text text = (Text) object;
+        return this.allTextElements.equals(text.allTextElements);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = prime;
+        result = result * prime * allTextElements.hashCode();
+        return result;
+    }
+
+    @Override
+    public String printText() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (TextElement element : allTextElements) {
+            stringBuilder.append(element.printText());
+        }
+        return stringBuilder.toString();
+    }
+}
