03c332907cecaf4a8c64c935ee444eb5f71f1ff8
Eugene Dubovik <dev.dubovik@gmail.com>
2022-03-27 18:52:05 +0300

    create Sentence

---
+++ src/main/java/com/epam/jwd/entity/paragraph/Sentence.java
@@ -0,0 +1,53 @@
+package com.epam.jwd.entity.paragraph;
+
+import com.epam.jwd.entity.TextElement;
+
+import java.util.List;
+
+public class Sentence implements TextElement {
+    private List<TextElement> words;
+
+    public List<TextElement> getWords() {
+        return words;
+    }
+
+    public Sentence(List<TextElement> words) {
+        this.words = words;
+    }
+
+    public void setWords(List<TextElement> words) {
+        this.words = words;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Sentence sentence = (Sentence) object;
+        return this.words.equals(sentence.getWords());
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = prime;
+        result = result * prime * words.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Sentence {" +
+                " words =" + words +
+                '}';
+    }
+
+    @Override
+    public String printText() {
+        return null;
+    }
+}
