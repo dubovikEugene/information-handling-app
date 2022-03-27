bb4393f0c914243cc13758cb06b8cf45061a9dae
Eugene Dubovik <dev.dubovik@gmail.com>
2022-03-27 18:53:34 +0300

    create ReaderFromFile

---
+++ src/main/java/com/epam/jwd/logic/reader/ReaderFromFile.java
@@ -0,0 +1,23 @@
+package com.epam.jwd.logic.reader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReaderFromFile {
+    private static final String OUTPUT_PATH = "src/main/resources/input/text.txt";
+
+    public void write() {
+
+        StringBuilder textFromFile = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(OUTPUT_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                textFromFile.append(line).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+//            TODO logger
+            e.printStackTrace();
+        }
+    }
+}
