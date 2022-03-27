807252a085d1431c2e1bbd5b80db870406640e78
Eugene Dubovik <dev.dubovik@gmail.com>
2022-03-27 20:19:40 +0300

    create WriterToFile

---
+++ src/main/java/com/epam/jwd/logic/writer/WriterToFile.java
@@ -0,0 +1,46 @@
+package com.epam.jwd.logic.writer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class WriterToFile {
+    private static final String DELETE_FILE_FROM_PATH_REG_EX = "/[a-zA-Z_]+\\.[a-zA-Z]{1,5}$";
+    private static final String REPLACEMENT_REG_EX = "";
+
+    public void write(String content, String outputPath) {
+
+        createDirectoryIsNotExist(outputPath);
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputPath));) {
+            bufferedWriter.write(content);
+        } catch (IOException e) {
+//            TODO logger
+            e.printStackTrace();
+        }
+    }
+
+    private void createDirectoryIsNotExist(String path) {
+        String pathWithoutFile = deleteFileFromPath(path);
+        Path pathOut = Paths.get(pathWithoutFile);
+        if (isPathNotExist(pathOut)) {
+            try {
+                Files.createDirectories(pathOut);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private boolean isPathNotExist(Path path) {
+        return Files.notExists(path);
+    }
+
+    private String deleteFileFromPath(String path) {
+        return path.replaceAll(DELETE_FILE_FROM_PATH_REG_EX, REPLACEMENT_REG_EX);
+    }
+}
