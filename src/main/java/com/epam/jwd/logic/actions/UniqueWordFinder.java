d9e525e0ed4d6f05453b406f90f5cd3a4df51361
Eugene Dubovik <dev.dubovik@gmail.com>
2022-03-31 21:52:39 +0300

    refactor code with using streams

--- src/main/java/com/epam/jwd/logic/actions/UniqueWordFinder.java
+++ src/main/java/com/epam/jwd/logic/actions/UniqueWordFinder.java
@@ -6,75 +6,60 @@ import com.epam.jwd.entity.paragraph.Sentence;
 import com.epam.jwd.entity.paragraph.Text;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class UniqueWordFinder {
-    private static final String WORD_REG_EX = "\\b%s\\b";
 
-    public String findUniqueWord(Text text) {
-        List<String> words = getArrayWordsFromFirstSentence(text);
-        String firstSentence = getFirstSentence(words);
-        String textWithoutFirstSentence = getTextWithoutFirstSentence(text, firstSentence);
-
-        List<String> regexList = getWordsRegex(words);
-        Pattern pattern;
-        for (String regex : regexList) {
-            pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(textWithoutFirstSentence.toLowerCase());
-            if (!matcher.find()) {
-                matcher = pattern.matcher(firstSentence.toLowerCase());
-                if (matcher.find()) {
-                    return matcher.group();
-                }
-            }
-
-        }
-        throw new NoSuchElementException("do not have words");
-    }
-
-    private List<String> getArrayWordsFromFirstSentence(Text text) {
+    public List<TextElement> findUniqueWord(Text text) {
         List<TextElement> textElements = text.getAllTextElements();
-        List<String> words = new ArrayList<>();
-        for (TextElement textElement : textElements) {
-            if (textElement.getClass().equals(Paragraph.class)) {
-                List<TextElement> sentence = ((Paragraph) textElement).getSentences();
-                TextElement element = sentence.get(0);
-                if (element.getClass().equals(Sentence.class)) {
-                    sentence = ((Sentence) element).getWords();
-                    for (TextElement word : sentence) {
-                        words.add(word.printText());
-                    }
-                }
 
-            }
-            break;
-        }
-        return words;
+        List<TextElement> sentencesList = getListOfSentence(textElements);
+        Sentence firstSentence = getFirstSentence(sentencesList);
+
+        List<TextElement> wordsFromFirstSentence = firstSentence.getWords();
+        List<TextElement> sentencesWithoutFirst = getSentencesListWithoutFirst(sentencesList);
+        List<TextElement> wordsFromWholeSentencesWithoutFirst = sentencesWithoutFirst.stream()
+                .filter(element -> element instanceof Sentence)
+                .map(sentence -> ((Sentence) sentence).getWords())
+                .flatMap(Collection::stream)
+                .toList();
+
+        List<TextElement> uniqueWords = new ArrayList<>();
+        wordsFromFirstSentence.stream()
+                .filter(word -> isHaveWord(word.printText(), wordsFromWholeSentencesWithoutFirst))
+                .forEach(uniqueWords::add);
+
+
+        return uniqueWords;
+
     }
 
-    private String getFirstSentence(List<String> words) {
-        StringBuilder firstSentence = new StringBuilder();
-        for (String word : words) {
-            firstSentence.append(word);
-            firstSentence.append(" ");
-        }
-        firstSentence.deleteCharAt(firstSentence.length() - 1);
-        return firstSentence.toString();
+    private boolean isHaveWord(String wordFromFirst, List<TextElement> wordsFromWholeSentencesWithoutFirst) {
+        return wordsFromWholeSentencesWithoutFirst.stream()
+                .noneMatch(word -> word.printText().toLowerCase().equals(wordFromFirst.toLowerCase()));
     }
 
-    private String getTextWithoutFirstSentence(Text text, String firstSentence) {
-        return text.printText().replaceFirst(firstSentence.toString(), "");
+    private List<TextElement> getListOfSentence(List<TextElement> textElements) {
+
+        List<TextElement> sentences = textElements.stream()
+                .filter(textElement -> textElement instanceof Paragraph)
+                .map(sentence -> ((Paragraph) sentence).getSentences())
+                .flatMap(Collection::stream)
+                .toList();
+
+        return sentences;
     }
 
-    private List<String> getWordsRegex(List<String> words) {
-        List<String> regexList = new ArrayList<>();
-        for (String word : words) {
-            String strRegex = String.format(WORD_REG_EX,word);
-            regexList.add(strRegex);
-        }
-        return regexList;
+    private Sentence getFirstSentence(List<TextElement> sentencesList) {
+        return (Sentence) sentencesList.get(0);
     }
+
+    private List<TextElement> getSentencesListWithoutFirst(List<TextElement> sentencesList) {
+        List<TextElement> sentencesListWithoutFirst = new ArrayList<>(List.copyOf(sentencesList));
+        sentencesListWithoutFirst.remove(0);
+        return sentencesListWithoutFirst;
+    }
+
+
 }
